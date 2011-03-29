90e3ab754694e9c1dbeb3fd4c1c90b82b651471d
Yo Han <yohanko1@gmail.com>
2011-03-28 19:31:03 -0500

    testing

--- js/fdclosure/fd_closure.js
+++ js/fdclosure/fd_closure.js
@@ -8,13 +8,24 @@ function removeDups(str) {
 	return attr;
 }
 
-function FdPair(fdFrom, fdTo) { // both should be strings
+function FdPair(fdFrom, fdTo) {
+	this.fdFrom = null;
+	this.fdTo = null;
 	if (fdFrom.constructor === String && fdTo.constructor === String) {
 		this.fdFrom = removeDups(fdFrom);
 		this.fdTo = removeDups(fdTo);
 	} else { // assume set as args
 		this.fdFrom = fdFrom;
 		this.fdTo = fdTo;
+	}
+
+	this.values = function () {
+		return [this.fdFrom.values(), this.fdTo.values()];	
+	};
+
+	// not sure...
+	this.equals = function (fdPair) {
+		return deepEqual(this,fdPair,"");
 	}
 }
 
@@ -44,18 +55,30 @@ function reflexivity(fd) { // FdPair
 	// generate trivial fd's
 	var rv = new Array();
 	var fdFromArr = fd.values();
+	// note that we don't care about fdTo values
 	var fromLen =fdFromArr.length;
-	for (var i=0; i<fromLen; i++) {
-		for (var len=1; len<=fromLen; len++) {
-			fdFromArr[x]
-			rv.push(new FdPair(fd.fdFrom, fd.fdTo))
+	for (var start=0; start<fromLen; start++) {
+		for (var end=start+1; end<=fromLen; end++) {
+			var to = fdFromArr.slice(start, end).join("");
+			rv.push(new FdPair(fd.fdFrom.join(""), to));
 		}
 	}
 	return rv;
 }
 
 function augmentation(fd, attr) { // fd: hashSet, attr: hashSet of attributes
+	var attrArr = attr.values();
+	var attrLen = attrArr.length();
+	var from = fd.fdFrom.values().join("");
+	var to = fd.fdTo.values().join("");
 
+	for (var start=0; start<attrLen; start++) {
+		for (var end=start+1; end<=attrLen; end++) {
+			// doc says shallow copy!??! but looks like deep copy...
+			rv.push(new FdPair(
+				from.concat(attrArr.slice(start,end)),to));
+		}
+	}
 }
 
 function transivity(fd_A, fd_B) { // args are FdPair
