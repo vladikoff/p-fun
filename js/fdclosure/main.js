521d159d2430bb7f37fc20bc85cd5d156e8f891f
yohanko1 <yohanko1@gmail.com>
2011-03-31 20:20:35 -0500

    input done

--- js/fdclosure/main.js
+++ js/fdclosure/main.js
@@ -37,17 +37,17 @@ function processInput(fdToIdPrefix, fdFromIdPrefix) {
 }
 
 
-function addField(area,fromField,toField,limit) {
-	if(!document.getElementById)
-		return; //Prevent older browsers from getting any further.
+function addField(area,fromField,toField, field, limit) {
+
 	var field_area = document.getElementById(area);
-	var all_inputs = field_area.getElementsByTagName("input"); //Get all the input fields in the given area.
-	//Find the count of the last element of the list. It will be in the format '<field><number>'. If the
-	//		field given in the argument is 'friend_' the last id will be 'friend_4'.
+	var all_inputs = field_area.getElementsByTagName("input");
 	var last_item = all_inputs.length - 1;
 	var last = all_inputs[last_item].id;
 	var count = Number(last.split("_")[1]) + 1;
-
+	
+	var next = (new String(field.id)).split("_")[0] + "_" + (count);
+	if(document.getElementById(next))
+		return;
 	//If the maximum number of elements have been reached, exit the function.
 	//		If the given limit is lower than 0, infinite number of fields can be created.
 	if(count > limit && limit > 0)
@@ -59,13 +59,24 @@ function addField(area,fromField,toField,limit) {
 		fdFrom.type = "text";
 		fdFrom.id = fromField+count;
 		fdFrom.name = fromField+count;
-
-		var maps = document.createTextNode(" -> ")
+		fdFrom.onkeyup = function() {
+			validate(this);
+		}; 
+		var maps = document.createElement("font");
+		maps.setAttribute("face","Symbol");
+		maps.appendChild(document.createTextNode(" \u2192 "));
+		//<font face="Symbol">&rarr;</font>
 
 		var fdTo = document.createElement("input");
 		fdTo.type = "text";
 		fdTo.id = toField+count;
 		fdTo.name = toField+count;
+		fdTo.onkeyup = function() {
+			validate(this);
+		};
+		fdTo.onfocus =function() {
+			addField('fd_list','fdFrom_','fdTo_', this, 20);
+		}; 
 
 		var fdRemove = document.createElement("a");
 		fdRemove.textContent = "Remove FD";
@@ -95,3 +106,32 @@ function output(outputFieldId, fdPairArr) {
 		document.getElementById(outputFieldId).appendChild(elem);
 	}
 }
+
+function validate(input)
+{
+    input.value = input.value.toUpperCase();
+        
+    var str = input.value;
+    var str2 = str;
+    var len = str.length;
+            
+    // Text Validation
+    for(var i=0; i<len; i++)
+    {
+        var c = str2.charAt(i);
+        if(!(c >= 'A' && c <= 'Z'))   
+        {
+           str = str.replace(c, "");
+        }
+    }
+    input.value = str;
+    //             
+    // // Symbol Validation
+    // if((len > 1 && str.charAt(len-1) == ',') || (len > 2 && str.charAt(len-2)== '-' && str.charAt(len-1)=='>'))
+    // {
+    //    txt.value = str.replace(",", "").replace("->","");
+    //    add();
+    //    document.getElementById("txt" + num).focus();
+    // }
+}
+
